4431a65e238b705fdff1a65d79ca43f67d731f76
webashrafians <webashrafians@gmail.com>
2020-10-17 04:48:53 +0600

    creative agency server done

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require("express");
+const app = express();
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
-const MongoClient = require("mongodb").MongoClient;
-require("dotenv").config();
 
 // mongodb connection............................
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g7kps.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 // creative agency server functionality..........
-const app = express();
-
+app.use(express.static("service"));
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static("service"));
 app.use(fileUpload());
 
 //home directory.................................
@@ -21,84 +20,134 @@ app.get("/", (req, res) => {
   res.send("Creative Agency mongodb and heroku is connected");
 });
 
-//server port.....................................
+//server port........................
 const port = 5000;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-//collection name.................................
 client.connect((err) => {
-  const ordersCollection = client.db("creativeAgency").collection("orders");
-  const serviceCollection = client.db("creativeAgency").collection("services");
-  const reviewCollection = client.db("creativeAgency").collection("reviews");
-  const adminCollection = client.db("creativeAgency").collection("admins");
+  const serviceCollection = client.db("creativeAgency").collection("service");
+  const reviewCollection = client.db("creativeAgency").collection("review");
+  const orderCollection = client.db("creativeAgency").collection("order");
+  const adminCollection = client.db("creativeAgency").collection("admin");
 
-  //addOrder........................................
-  app.post("/addOrder", (req, res) => {
-    const orders = req.body;
-    ordersCollection.insertOne(orders).then((result) => {
-      res.send(result.insertedCount > 0);
-    });
-  });
-
-  //addReview........................................
+  // addReview..........................
   app.post("/addReview", (req, res) => {
-    const reviews = req.body;
-    reviewCollection.insertOne(reviews).then((result) => {
+    const review = req.body;
+    reviewCollection.insertOne(review).then((result) => {
       res.send(result.insertedCount > 0);
     });
   });
 
-  //addAdminr........................................
-  app.post("/addAdmin", (req, res) => {
-    const admins = req.body;
-    adminCollection.insertOne(admins).then((result) => {
-      res.send(result.insertedCount > 0);
+  //reviewList..........................
+  app.get("/reviewList", (req, res) => {
+    reviewCollection.find({}).toArray((error, document) => {
+      res.send(document);
     });
   });
 
-  //addService........................................
-  app.post("/addService", (req, res) => {
+  //addOrder...........................
+  app.post("/addOrder", (req, res) => {
     const file = req.files.file;
-    const service = req.body.service;
-    const details = req.body.details;
+    const name = req.body.name;
+    const email = req.body.email;
+    const projectTitle = req.body.projectTitle;
+    const projectDetails = req.body.projectDetails;
+    const price = req.body.price;
+    const status = req.body.status;
     const newImg = file.data;
     const encImg = newImg.toString("base64");
+
+    console.log(file, name, email, projectTitle, projectDetails, price, status);
 
     var image = {
       contentType: file.mimetype,
       size: file.size,
       img: Buffer.from(encImg, "base64"),
     };
-    serviceCollection.insertOne({ service, details, image }).then((result) => {
+
+    orderCollection
+      .insertOne({
+        name,
+        email,
+        projectTitle,
+        projectDetails,
+        price,
+        status,
+        image,
+      })
+      .then((result) => {
+        res.send(result.insertedCount > 0);
+      });
+  });
+
+  //OrderCard...........................
+  app.post("/orderCard", (req, res) => {
+    const email = req.body.email;
+    orderCollection.find({ email: email }).toArray((error, document) => {
+      res.send(document);
+    });
+  });
+
+  //fullOrderList..........................
+  app.get("/fullOrderList", (req, res) => {
+    orderCollection.find({}).toArray((error, document) => {
+      res.send(document);
+    });
+  });
+
+  //addService...........................
+  app.post("/addService", (req, res) => {
+    const file = req.files.file;
+    const title = req.body.title;
+    const details = req.body.details;
+    const newImg = file.data;
+    const encImg = newImg.toString("base64");
+
+    // console.log(file, title, details);
+    var image = {
+      contentType: file.mimetype,
+      size: file.size,
+      img: Buffer.from(encImg, "base64"),
+    };
+
+    serviceCollection.insertOne({ title, details, image }).then((result) => {
       res.send(result.insertedCount > 0);
     });
   });
 
-  //get services.........................................
-  app.get("/services", (req, res) => {
-    serviceCollection.find({}).toArray((err, documents) => {
-      res.send(documents);
+  //service..........................
+  app.get("/service", (req, res) => {
+    serviceCollection.find({}).toArray((error, document) => {
+      res.send(document);
     });
   });
 
-  //orderLists ...........................................
-  app.get("/orderLists", (req, res) => {
-    const services = req.body;
+  //addAdmin...........................
+  app.post("/addAdmin", (req, res) => {
+    const admin = req.body;
+    // console.log(review);
+    adminCollection.insertOne(admin).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  //admin..........................
+  app.get("/admin", (req, res) => {
+    adminCollection.find({}).toArray((error, document) => {
+      res.send(document);
+    });
+  });
+  
+  //isAdmin...........................
+  app.post("/isAdmin", (req, res) => {
     const email = req.body.email;
-    adminCollection.find({ email: email }).toArray((err, admins) => {
-      if (admins.lenth === 0) {
-        filter.email = email;
-      }
-      ordersCollection.find().toArray((err, documents) => {
-        console.log(email, admins, documents);
-        res.send(documents);
-      });
+    adminCollection.find({ email: email }).toArray((error, admin) => {
+      res.send(admin.length > 0);
     });
   });
-});
 
+});
 app.listen(process.env.PORT || port);
